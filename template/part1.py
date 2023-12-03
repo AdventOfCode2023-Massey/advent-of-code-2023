84bfb9322bc9a5f391543daa3fda4d148dfce45e
Bart Massey <bart.massey@gmail.com>
2023-12-03 12:37:12 -0800

    added part templates

---
+++ template/part1.py
@@ -0,0 +1,4 @@
+# Advent of Code <year> Day <day> Part 1
+# Code by Github Copilot Chat
+# Editing by Bart Massey
+
---
+++ template/part2.py
@@ -0,0 +1,4 @@
+# Advent of Code <year> Day <day> Part 2
+# Code by Github Copilot Chat
+# Editing by Bart Massey
+
